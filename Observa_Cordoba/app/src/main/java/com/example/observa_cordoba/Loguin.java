70b3495092b5bfc2ecea62fa82c02cb5ebc42168
SalvadorCasas <salvadorcasas11@gmail.com>
2023-06-14 20:18:15 -0300

    Agrego front de loguin

---
+++ Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Loguin.java
@@ -0,0 +1,14 @@
+package com.example.observa_cordoba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Loguin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loguin);
+    }
+}
