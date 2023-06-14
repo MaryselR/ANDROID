92944f00bc4350703a628237ff4b0e585b47f588
SalvadorCasas <salvadorcasas11@gmail.com>
2023-06-14 13:02:41 -0300

    Agrego video a seccion Inicio

---
+++ Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Inicio.java
@@ -0,0 +1,35 @@
+package com.example.observa_cordoba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Inicio extends AppCompatActivity {
+    private VideoView video;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        /*Ocultamos la barra de titulo de la app si el video se ve en modo horizontal */
+        int orientacion=getResources().getConfiguration().orientation;
+        if(orientacion== Configuration.ORIENTATION_LANDSCAPE){
+            getSupportActionBar().hide();
+        }
+        else{
+            getSupportActionBar().show();
+        }
+
+        /*Añadimos el video y su controlador*/
+        video=findViewById(R.id.video);
+        video.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.videoandroid));
+        MediaController mc = new MediaController(this);
+        video.setMediaController(mc);
+        mc.setAnchorView(video);
+        video.start();
+    }
+}
