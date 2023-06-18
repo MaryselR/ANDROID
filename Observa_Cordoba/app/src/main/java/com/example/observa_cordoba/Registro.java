f0de8fbb08efbec8377510a3d4aa2dff68fed525
SalvadorCasas <salvadorcasas11@gmail.com>
2023-06-18 20:42:45 -0300

    Agrego logica de registrar usuario

--- Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
+++ Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
@@ -4,13 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import OpenHelper.SQLite_OpenHelper;
 
 public class Registro extends AppCompatActivity {
+
+    Button confirmar;
+    EditText usuario,correo,contrasenia;
+
+    SQLite_OpenHelper helper=new SQLite_OpenHelper(this,"MiObservador",null,1);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        confirmar = (Button) findViewById(R.id.btnConfirmar);
+        usuario = (EditText) findViewById(R.id.inputUsuario);
+        correo = (EditText) findViewById(R.id.inputEmail);
+        contrasenia = (EditText) findViewById(R.id.inputContrasenia);
+
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helper.abrir();
+                helper.insertarRegistro(String.valueOf(usuario.getText()),String.valueOf(correo.getText()),String.valueOf(contrasenia.getText()));
+                helper.cerrar();
+
+                Toast.makeText(getApplicationContext(),"Registro exitoso",Toast.LENGTH_LONG).show();
+
+                Intent i = new Intent(getApplicationContext(), Inicio.class);
+                startActivity(i);
+            }
+        });
     }
     public void MainActivity(View vista) {
         Intent mainIntent = new Intent(this, MainActivity.class);
