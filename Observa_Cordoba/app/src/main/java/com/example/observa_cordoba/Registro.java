4ac3057918ccce6bf3bd9346ae5993575cce5718
MaryselR <69443638+MaryselR@users.noreply.github.com>
2023-06-19 06:26:55 -0300

    Correccion de error de DB. Correccion de Login

--- Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
+++ Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
@@ -1,5 +1,6 @@
 package com.example.observa_cordoba;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.provider.ContactsContract;
@@ -9,19 +10,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import OpenHelper.SQLite_OpenHelper;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class Registro extends AppCompatActivity {
 
     Button confirmar;
-    EditText usuario,correo,contrasenia, txtUsuario;
+    EditText usuario,correo,contrasenia;
 
-    SQLite_OpenHelper helper=new SQLite_OpenHelper(this,"MiObservador",null,1);
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        miObservadorDB dbHelper = new miObservadorDB(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         confirmar = (Button) findViewById(R.id.btnConfirmar);
         usuario = (EditText) findViewById(R.id.ingresoUsuario);
@@ -32,23 +43,54 @@ public class Registro extends AppCompatActivity {
             @Override
 
             public void onClick(View v) {
-                helper.abrir();
-                helper.insertarRegistro(String.valueOf(usuario.getText()),String.valueOf(correo.getText()),String.valueOf(contrasenia.getText()));
-                helper.cerrar();
 
-                Toast.makeText(getApplicationContext(),"Registro exitoso",Toast.LENGTH_LONG).show();
-                String message = usuario.getText().toString();
+                String usuarioNuevo = usuario.getText().toString();
+                String correoNuevo = correo.getText().toString();
+                String contraseniaNueva = contrasenia.getText().toString();
 
-                Intent i = new Intent(getApplicationContext(), Inicio.class);
-               startActivity(i);
-                Intent inicioIntent = new Intent(getApplicationContext(), Inicio.class);
-               inicioIntent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(inicioIntent);
+                if (db != null) {
 
-            }
+                    // Insert con ContentValues
+                    ContentValues cv = new ContentValues();
+                    cv.put("usuario", usuarioNuevo);
+                    cv.put("correo", correoNuevo);
+                    cv.put("contrasenia", contraseniaNueva);
+                    db.insert("usuarios", null, cv);
 
+                    }
+                    if (usuarioNuevo.isEmpty() || correoNuevo.isEmpty() || contraseniaNueva.isEmpty()) {
+                        Toast.makeText(Registro.this, "Ingresar datos requeridos", Toast.LENGTH_LONG).show();
+                        return;
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Registro exitoso", Toast.LENGTH_LONG).show();
+                        }
+
+                    String message = usuario.getText().toString();
+
+                    Intent i = new Intent(getApplicationContext(), Inicio.class);
+                    startActivity(i);
+
+                    Intent inicioIntent = new Intent(getApplicationContext(), Inicio.class);
+                    inicioIntent.putExtra(EXTRA_MESSAGE, message);
+                    startActivity(inicioIntent);
+                }
         });
+    }
 
+    private void regUser(String emailUser, String passwordUser) {
+        mAuth.signInWithEmailAndPassword(emailUser, passwordUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(Registro.this, MainActivity.class));
+                    Toast.makeText(Registro.this, "¡Bienvenido!", Toast.LENGTH_LONG).show();
+                } else {
+                    FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                    Toast.makeText(Registro.this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
     public static final String EXTRA_MESSAGE =
             "com.example.android.Observa_Cordoba.Registro.extra.MESSAGE";
