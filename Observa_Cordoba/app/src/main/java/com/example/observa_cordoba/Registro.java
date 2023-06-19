ea0fb82a862eaad1b16fd07ada478e6dc4b23797
MaryselR <69443638+MaryselR@users.noreply.github.com>
2023-06-18 22:54:45 -0300

    Arreglo de paso de datos entre activities

--- Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
+++ Observa_Cordoba/app/src/main/java/com/example/observa_cordoba/Registro.java
@@ -14,7 +14,7 @@ import OpenHelper.SQLite_OpenHelper;
 public class Registro extends AppCompatActivity {
 
     Button confirmar;
-    EditText usuario,correo,contrasenia;
+    EditText usuario,correo,contrasenia, txtUsuario;
 
     SQLite_OpenHelper helper=new SQLite_OpenHelper(this,"MiObservador",null,1);
 
@@ -24,51 +24,37 @@ public class Registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
 
         confirmar = (Button) findViewById(R.id.btnConfirmar);
-        usuario = (EditText) findViewById(R.id.inputUsuario);
-        correo = (EditText) findViewById(R.id.inputEmail);
-        contrasenia = (EditText) findViewById(R.id.inputContrasenia);
+        usuario = (EditText) findViewById(R.id.ingresoUsuario);
+        correo = (EditText) findViewById(R.id.ingresoEmail);
+        contrasenia = (EditText) findViewById(R.id.ingresoPassword);
 
         confirmar.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
                 helper.abrir();
                 helper.insertarRegistro(String.valueOf(usuario.getText()),String.valueOf(correo.getText()),String.valueOf(contrasenia.getText()));
                 helper.cerrar();
 
                 Toast.makeText(getApplicationContext(),"Registro exitoso",Toast.LENGTH_LONG).show();
+                String message = usuario.getText().toString();
 
                 Intent i = new Intent(getApplicationContext(), Inicio.class);
-                startActivity(i);
+               startActivity(i);
+                Intent inicioIntent = new Intent(getApplicationContext(), Inicio.class);
+               inicioIntent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(inicioIntent);
+
             }
+
         });
 
     }
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.Observa_Cordoba.Registro.extra.MESSAGE";
     public void MainActivity(View vista) {
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
     }
 
-    public void Inicio(View vista) {
-        Intent inicioIntent = new Intent(this, Inicio.class);
-        String message = txtUsuario.getText().toString();
-        inicioIntent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(inicioIntent);
-
-        String userName = registroUsuario.getText().toString().trim();
-        String userPassword = registroPassword.getText().toString().trim();
-        String userEmail = registroEmail.getText().toString().trim();
-
-        if (userName.isEmpty() || userPassword.isEmpty() || userEmail.isEmpty()) {
-            Toast.makeText(Registro.this, "Ingresar datos requeridos", Toast.LENGTH_LONG).show();
-        } else {
-            nuevoUsuario(userName, userEmail, userPassword);
-        }
-    }
-
-    public static final String EXTRA_MESSAGE =
-            "com.example.android.Observa_Cordoba.Registro.extra.MESSAGE";
-           /* "com.example.android.registroUsuario.extra.MESSAGE";*/
-    private void nuevoUsuario(String userName, String userEmail, String userPassword){
-
-    }
 }
